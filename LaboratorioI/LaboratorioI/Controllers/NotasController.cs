42cb13420c1cb160fcddfa0a298605c035289c5c
ha0372 <69734226+ha0372@users.noreply.github.com>
2021-08-27 12:09:48 -0600

    index

---
+++ LaboratorioI/LaboratorioI/Controllers/NotasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaboratorioI.Controllers
+{
+    public class NotasController : Controller
+    {
+        // GET: Notas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CalculoNotas(string nombre, double lab1, double lab2 , double lab3, double par1, double par2, double par3)
+        {
+            return View();
+        }
+    }
+}
---
+++ LaboratorioI/LaboratorioI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LaboratorioI.MvcApplication" Language="C#" %>
